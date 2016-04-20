3e9758aa492e11e912df1b33c1ef754013ff95f3
Mike Brown <m@mikecb.org>
2016-04-20 15:58:27 -0400

    Cli interface, reading from file, sid generation.

--- godns2snort.go
+++ godns2snort.go
@@ -5,6 +5,8 @@ import (
 	"fmt"
 	"os"
 	"strings"
+
+	"github.com/codegangsta/cli"
 )
 
 func readLines(path string) ([]string, error) {
@@ -38,21 +40,49 @@ func writeLines(lines []string, path string) error {
 }
 
 func main() {
-	domains, _ := readLines("dns.txt")
-	for _, domain := range domains {
-		//Remove leading stops which often preceed TLDs.
-		trimmed := strings.TrimPrefix(domain, ".")
-		//Split domain into slice of segments.
-		segments := strings.Split(trimmed, ".")
-		i := 0
-		rulefragment := ""
-		for _, s := range segments {
-			shex := fmt.Sprintf("%02X", len(s))
-			rulefragment += shex + "|" + s + "|"
-			i++
-		}
+	var in, out string
+	var sid int
 
-		fmt.Printf("alert udp $HOME_NET any -> $EXTERNAL_NET 53 (msg:\"BLACKLIST DNS domain %s\"; flow:to_server; byte_test:1,!&,0xF8,2; content:\"|%s00|\"; fast_pattern:only; metadata:service dns;  sid:; rev:1;)\n", domain, rulefragment)
+	app := cli.NewApp()
+	app.Name = "godns2snort"
+	app.Usage = "convert a list of domains into snort reject rules."
 
+	app.Flags = []cli.Flag{
+		cli.StringFlag{
+			Name:        "input, i",
+			Value:       "dns.txt",
+			Usage:       "path to a newline separated list of domains.",
+			Destination: &in},
+		cli.StringFlag{
+			Name:        "output, o",
+			Value:       "snort.rules",
+			Usage:       "path to output rules file.",
+			Destination: &out},
+		cli.IntFlag{
+			Name:        "sid, s",
+			Value:       1000000,
+			Usage:       "a number at which to begin the sequential rule numbering.",
+			Destination: &sid},
 	}
+
+	app.Action = func(c *cli.Context) {
+		domains, _ := readLines(in)
+		for _, domain := range domains {
+			//Remove leading stops which often preceed TLDs.
+			trimmed := strings.TrimPrefix(domain, ".")
+			//Split domain into slice of segments.
+			segments := strings.Split(trimmed, ".")
+			i := 0
+			rulefragment := ""
+			for _, s := range segments {
+				shex := fmt.Sprintf("%02X", len(s))
+				rulefragment += shex + "|" + s + "|"
+				i++
+			}
+
+			fmt.Printf("alert udp $HOME_NET any -> $EXTERNAL_NET 53 (msg:\"BLACKLIST DNS domain %s\"; flow:to_server; byte_test:1,!&,0xF8,2; content:\"|%s00|\"; fast_pattern:only; metadata:service dns;  sid:%d; rev:1;)\n", domain, rulefragment, sid)
+			sid++
+		}
+	}
+	app.Run(os.Args)
 }
